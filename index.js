60b8c8b1caee3d53d8eee7c5ea0341dd776e7d15
Chad Walker <chad@athenascope.com>
2022-02-23 11:14:40 -0600

    basic

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import Express from 'express';
 
 const app = new Express();
 
+const birth = new Date('1976-08-01T21:01:00-0700');
+const baseline_years = 87;
+const risk = 3;
+const health = -5;
+const years = baseline_years + (health * baseline_years / 100) + (risk * baseline_years / 100);
+const time_ms = years * 365.25 * 24 * 60 * 60 * 1000;
+const end = new Date(birth.getTime() + time_ms);
+
 app.get('/', (req, res) => {
-  res.end('Hello World!');
+  res.end(`So Far: ${100 * (Date.now() - birth.getTime()) / time_ms}`);
 });
 
 app.listen(80);
